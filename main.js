0b8de8966f74a892221619a2018adcbd477507c6
DavidWangDong <190780806@qq.com>
2017-11-06 18:03:23 +0800

    初始化

--- main.js
+++ main.js
@@ -16,14 +16,28 @@ window.hammer = hm;
 	})
 	requireList.validator();
 	// 事件对象
-	// 事件定制，事件移除
-	
+	// 事件定制
+	class Eventer  {
+		constructor (){
+
+		}
+	}
+
 	// 预览对象(即选择预览框)
 	// 事件处理
+	class Previewer {
+		constructor () {
 
+		}
+	}
 	// 剪切对象:包括1、图片对象，2、遮罩层对象
 	// 事件处理
 
+	// 图片对象
+
+	// 遮罩层对象
+
+	
 	// 图片生成对象
 
 })(window)
